38da84ef194977ccd6c1a9a5b6f1f00ca1a16533
XanChoon <smithchoon@gmail.com>
2018-08-13 20:42:35 +0800

    up

---
+++ multiply.js
@@ -0,0 +1,11 @@
+function multiply(){  
+    var txt;
+    var number1 = parseInt(prompt("Please enter your number", ""));
+	var number2 = parseInt(prompt("Please enter your number", ""));
+	var answer = number1 * number2;
+    if (number1 && number2 == null || number1 && number2 == "") {
+        txt = "User cancelled the prompt.";
+    } else {
+       alert("The answer is " + answer);
+    }
+}
